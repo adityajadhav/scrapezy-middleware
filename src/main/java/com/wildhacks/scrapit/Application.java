c6ecd2c7cebef8ba4bed758677f4f99ddba49a3e
Aditya Jadhav <aditya.jadhav27@gmail.com>
2017-11-04 11:39:42 -0500

    Adding default user

--- src/main/java/com/wildhacks/scrapit/Application.java
+++ src/main/java/com/wildhacks/scrapit/Application.java
@@ -1,17 +1,38 @@
 package com.wildhacks.scrapit;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wildhacks.scrapit.data.User;
+import com.wildhacks.scrapit.repo.UserRepository;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+		
+		User a = new User();
+		a.setName("Aditya J");
+
+		userRepository.save(a);
+
+		List<User> users = userRepository.findAll();
+		for (User user : users) {
+			System.out.println(user.getName());
+		}
 
 	}
 
--- src/main/java/com/wildhacks/scrapit/test/Test.java
+++ src/main/java/com/wildhacks/scrapit/test/Test.java
@@ -1,3 +1,4 @@
+package com.wildhacks.scrapit.test;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
